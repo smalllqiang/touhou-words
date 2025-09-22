1a690331934f5691d7a5e5c65bace8310ad99bf1
smalllqiang <F11F10E8@outlook.com>
2025-09-22 23:31:50 +0800

    去除前後空格

--- process.py
+++ process.py
@@ -3,6 +3,7 @@
 # 依Unicode排序
 # 去除空行
 # 去重
+# 去除可能存在的前後空格
 # 備份原文件
 
 import time
@@ -12,8 +13,15 @@ shutil.copy("data.txt", f"data_{int(time.time())}.txt")
 
 with open("data.txt", mode="r", encoding="utf-8") as f:
     lines = f.readlines()
+    # 去除前後空格
+    length = len(lines)
+    for j in range(length):
+        lines[j] =  lines[j].strip() + "\n"
+    # 去重
     lines = list(set(lines))
+    # 排序
     lines.sort()
+    # 去除空行
     length = len(lines)
     for i in range(length):
         if lines[length - 1 - i] == "\n":
